4ec0617f3d0f07cb7dec17ab66a4fb5b88035835
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-06-29 14:06:31 +0200

    get activities and get legs

--- analyse/data.py
+++ analyse/data.py
@@ -3,6 +3,18 @@ import os
 import pandas as pd
 
 
+def get_activities(folder):
+    filename = os.path.join(folder, "matsim_activities.txt")
+    return pd.DataFrame.from_csv(filename, sep="\t")
+
+
+def get_legs(folder):
+    filename = os.path.join(folder, "matsim_trips.txt")
+    legs = pd.DataFrame.from_csv(filename, sep="\t")
+    legs["duration"] = legs.end_time-legs.start_time
+    return legs
+
+
 def get_trips(folder):
     filename = os.path.join(folder, "matsim_journeys.txt")
     return pd.DataFrame.from_csv(filename, sep="\t")
