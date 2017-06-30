ccbc2cdff35733a9f03fe0e1e4b4dc354bbdeee9
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-06-30 13:33:50 +0200

    merge_journeys_with_acts

--- analyse/data.py
+++ analyse/data.py
@@ -30,3 +30,13 @@ def get_legs(folder):
 def get_legs_sum(folder):
     legs = get_legs(folder)
     return legs.groupby("mode").sum()[["distance", "duration"]]
+
+
+def merge_journeys_with_acts(acts, journeys):
+    _acts = acts[["type", "zone", "activity_id"]]
+    _acts = _acts.rename(columns={'type': 'act_type', 'zone': 'act_zone'})
+
+    merged = journeys.merge(_acts, left_on="from_act", right_on="activity_id")
+    merged = merged.merge(_acts, left_on="to_act", suffixes=("_from", "_to"), right_on="activity_id")
+    return merged[np.logical_and(merged.act_zone_from != "undefined", merged.act_zone_to != "undefined" )]
+
