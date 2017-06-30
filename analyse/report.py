3da7c57c084a3cab27a31aef402f4290fa84411c
Metrailler Denis <u222223@i89837.sbb.ch>
2017-06-30 14:00:40 +0200

    geojson must be given

--- analyse/report.py
+++ analyse/report.py
@@ -14,7 +14,7 @@ def insert_fig(worksheet, fig, row, col, kwargs):
         return t.name
 
 
-def make_report(path, acts, geojson):
+def make_report(path, acts, geojson, rect=None):
     to_delete = []
     try:
 
