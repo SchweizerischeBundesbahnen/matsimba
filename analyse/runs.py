7488f8b11d296eac5084f4f453babb212aaae61f
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-09-12 16:34:32 +0200

    laod_Data in runs

--- analyse/runs.py
+++ analyse/runs.py
@@ -21,6 +21,14 @@ class RunsDatabase:
     def get_path(self, name):
         return self.db.loc[name].Path
 
+    def load_data(self, only_new=True):
+        for name in self.db.index:
+            if only_new:
+                if name not in self.data:
+                    self._load_data(name)
+            else:
+                self._load_data(name)
+
     def _load_data(self, run_name):
 
         keys = {"acts": {"path": "matsim_activities.txt", "sep": "\t"},
@@ -36,7 +44,7 @@ class RunsDatabase:
             try:
                 sep = keys[name]["sep"]
                 path = os.path.join(_path, keys[name]["path"])
-                self.data[name][run_name] = pd.DataFrame.from_csv(path, sep=sep, encoding="utf-8").reset_index()
+                self.data[run_name][name] = pd.DataFrame.from_csv(path, sep=sep, encoding="utf-8").reset_index()
             except Exception as e:
                 print e.message
 
