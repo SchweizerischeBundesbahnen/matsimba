642285c7a0ec61e3181857dcc4ae05e5aa1fbe2f
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-09-12 18:30:08 +0200

    merge whl enumber of trips

--- analyse/compare_runs/plot.py
+++ analyse/compare_runs/plot.py
@@ -14,8 +14,18 @@ def number_trips(runs, by=None):
     return ax, df
 
 
-def number_trips_by(runs, by):
-    df = analyse.compare.append([run.get_trips() for run in runs], [run.name for run in runs], column="Run")
+def number_trips_by(runs, by, other=None, on=None):
+    _dfs = []
+    _names = []
+    for run in runs:
+        _names.append(run.name)
+        _df = run.get_trips()
+        if other is not None:
+            _df2 = run.get(other)
+            _df.merge(_df2.set_index(on)[by], how="left", left_on=on, right_index=True)
+        _dfs.append(_df)
+
+    df = analyse.compare.append(_dfs, names=_names, column="Run")
     df = df.pivot_table(index=by, columns="Run", values="person_id", aggfunc="count", fill_value=0)
     ax = df.plot(kind="bar")
     return ax, df
