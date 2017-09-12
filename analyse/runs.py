a217efdf9639b0d94f075e16ce8a18e536ec5eef
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-09-12 17:30:41 +0200

    made a run class

--- analyse/runs.py
+++ analyse/runs.py
@@ -5,40 +5,52 @@ import logging
 import analyse.data
 
 
-class RunsDatabase:
-    def __init__(self, path):
+class Run:
+    def __init__(self, path, name, scale_factor):
         self.path = path
-        self.data = defaultdict(dict)
-        self.db = None
-        self.rescan()
+        self.name = name
+        self.scale_factor = scale_factor
 
-    def rescan(self):
-        db = pd.read_csv(self.path)
-        db.set_index(["Name"], inplace=True)
-        self.db = db
+        self.data = {}
 
-    def get_paths(self, names):
-        return [self.get_path(k) for k in names]
+    def get_persons(self):
+        return self.data["persons"]
 
-    def get_path(self, name):
-        return self.db.loc[name].Path
+    def get_acts(self):
+        return self.data["acts"]
 
-    def load_data(self, only_new=True, names=None, with_stop_points=False):
-        if names is None:
-            names = self.db.index
-        for name in names:
-            if only_new:
-                if name not in self.data:
-                    self._load_data(name, with_stop_points=with_stop_points)
-            else:
-                self._load_data(name, with_stop_points=with_stop_points)
+    def get_trips(self):
+        return self.data["journeys"]
 
-    def load_stop_points(self, run_name):
-        _path = self.get_path(run_name)
-        self.data[run_name]["stop_points"] = analyse.data.get_stops(_path)
+    def get_legs(self):
+        return self.data["legs"]
 
-    def _load_data(self, run_name, with_stop_points):
-        logging.info("Laoding Data for %s" % run_name)
+    def get_vehjourneys(self):
+        return self.data["vehjourneys"]
+
+    def get_boarding_alighting(self):
+        return self.data["stops"]
+
+    def get_stops(self):
+        return self.get_boarding_alighting()
+
+    def get_ea(self):
+        return self.get_boarding_alighting()
+
+    def get_linkvolumes(self):
+        return self.data["linkvolumes"]
+
+    def get_planelements(self):
+        return self.data["planelements"]
+
+    def get_stop_points(self):
+        return self.data["stop_points"]
+
+    def load_stop_points(self):
+        self.data["stop_points"] = analyse.data.get_stops(self.path)
+
+    def load_data(self, with_stop_points=False):
+        logging.info("Laoding Data for %s" % self.name)
         keys = {"acts":
                     {"path": "matsim_activities.txt", "sep": "\t"},
                 "journeys":
@@ -55,52 +67,53 @@ class RunsDatabase:
                     {"path": "plan_elements.csv", "sep": ";"},
                 "persons":
                     {"path": "agents.csv", "sep": ";"}}
-        _path = self.get_path(run_name)
         for name in keys:
             try:
                 sep = keys[name]["sep"]
-                path = os.path.join(_path, keys[name]["path"])
-                self.data[run_name][name] = pd.DataFrame.from_csv(path, sep=sep, encoding="utf-8").reset_index()
+                path = os.path.join(self.path, keys[name]["path"])
+                self.data[name] = pd.DataFrame.from_csv(path, sep=sep, encoding="utf-8").reset_index()
             except Exception as e:
                 logging.error(e.message)
         if with_stop_points:
-            self.load_stop_points(run_name)
-
-    def get(self, sim_name, name):
-        return self.data[sim_name][name]
-
-    def get_persons(self, sim_name):
-        return self.get(sim_name, "persons")
-
-    def get_acts(self, sim_name):
-        return self.get(sim_name, "acts")
-
-    def get_trips(self, sim_name):
-        return self.get(sim_name, "journeys")
-
-    def get_legs(self, sim_name):
-        return self.get(sim_name, "legs")
-
-    def get_vehjourneys(self, sim_name):
-        return self.get(sim_name, "vehjourneys")
-
-    def get_boarding_alighting(self, sim_name):
-        return self.get(sim_name, "stops")
-
-    def get_stops(self, sim_name):
-        return self.get_boarding_alighting(sim_name)
-
-    def get_ea(self, sim_name):
-        return self.get_boarding_alighting(sim_name)
-
-    def get_linkvolumes(self, sim_name):
-        return self.get(sim_name, "linkvolumes")
-
-    def get_planelements(self, sim_name):
-        return self.get(sim_name, "planelements")
-
-    def get_stop_points(self, sim_name):
-        return self.get(sim_name, "stop_points")
+            self.load_stop_points()
 
 
+class RunsDatabase:
+    def __init__(self, path):
+        self.path = path
+        self.runs = defaultdict(dict)
+        self.db = None
+        self.rescan()
+
+    def rescan(self):
+        db = pd.read_csv(self.path)
+        db.set_index(["Name"], inplace=True)
+        self.db = db
+
+    def get_paths(self, names):
+        return [self.get_path(k) for k in names]
+
+    def get_path(self, name):
+        return self.runs[name].path
+
+    def add_run(self, run):
+        print "not yet implemented sorry :("
+
+    def load_run(self, name, with_stop_points):
+        run = Run(name=name, path=self.db.loc[name].Path, scale_factor=self.db.loc[name].Factor)
+        run.load_data(with_stop_points)
+        self.runs[run.name] = run
+
+    def load_data(self, only_new=True, names=None, with_stop_points=False):
+        if names is None:
+            names = self.db.index
+        for name in names:
+            if only_new:
+                if name not in self.runs:
+                    self.load_run(name, with_stop_points)
+            else:
+                self.load_run(name, with_stop_points)
+
+    def get(self, sim_name):
+        return self.runs[sim_name]
 
