8a75e9b750b0aab4b38ca9a0d2a42d6709cf3dec
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-09-13 11:22:49 +0200

    add_run method

--- analyse/runs.py
+++ analyse/runs.py
@@ -96,8 +96,11 @@ class RunsDatabase:
     def get_path(self, name):
         return self.runs[name].path
 
-    def add_run(self, run):
-        print "not yet implemented sorry :("
+    def add_run(self, name, path, scale_factor, overwrite=False):
+        if not overwrite and name in self.db.index:
+            logging.info("Name %s already in csv. Overwrite?" % name)
+            return
+        self.runs[name] = Run(name, path=path, scale_factor=scale_factor)
 
     def load_run(self, name, with_stop_points):
         run = Run(name=name, path=self.db.loc[name].Path, scale_factor=self.db.loc[name].Factor)
