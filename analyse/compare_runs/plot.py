d49dd89b9a6aa86f6b5041607ff703113052f8db
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-09-13 11:17:45 +0200

    rename function to func

--- analyse/compare_runs/plot.py
+++ analyse/compare_runs/plot.py
@@ -14,24 +14,24 @@ def number_trips(runs, by=None):
     return ax, df
 
 
-def number_trips_by(runs, by, function=None):
+def number_trips_by(runs, by, func=None):
     return compare_by(runs, by=by, get_data=lambda r: r.get_trips(), title="#trips", values="person_id",
-               aggfunc="count", function=function)
+               aggfunc="count", func=func)
 
 
-def number_legs_by(runs, by, function=None):
+def number_legs_by(runs, by, func=None):
     return compare_by(runs, by=by, get_data=lambda r: r.get_legs(), title="#legs", values="trip_id",
-               aggfunc="count", function=function)
+               aggfunc="count", func=func)
 
 
-def compare_by(runs, by, get_data, function=None, title="#trips", values="person_id", aggfunc="count"):
+def compare_by(runs, by, get_data, func=None, title="#trips", values="person_id", aggfunc="count"):
     _dfs = []
     _names = []
     for run in runs:
         _names.append(run.name)
         _df = get_data(run)
-        if function is not None:
-            _df = function(_df, run)
+        if func is not None:
+            _df = func(_df, run)
         _dfs.append(_df)
 
     df = analyse.compare.append(_dfs, names=_names, column="Run")
