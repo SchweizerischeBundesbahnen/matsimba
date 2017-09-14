4cbdf83caebf67916b33d7c7c54497b95622fcfc
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-09-14 14:58:12 +0200

    remoe make_simba_code

--- analyse/etappen.py
+++ analyse/etappen.py
@@ -1,4 +1,5 @@
 from analyse.compare import _compare_by
+from haltepunkte import make_simba_code
 
 
 def pfahrt(runs, by, prozentual=False, func=None):
@@ -20,3 +21,20 @@ def pkm(runs, by, prozentual=False, func=None):
         df = df.div(df.sum())*100
     return df
 
+
+def _ea(runs, field, by=[], func=None):
+    def _func(df, run):
+        df = func(df, run)
+        return df
+
+    df = _compare_by(runs, [field+"_code"]+by, lambda r: r.get_legs(), func=_func, values=field, aggfunc="count")
+    df = df.multiply([r.scale_factor for r in runs])
+    return df
+
+
+def einsteiger(runs, by=[], func=None):
+    return _ea(runs, "boarding_stop", by, func)
+
+
+def austeiger(runs, by=[], func=None):
+    return _ea(runs, "alighting_stop", by, func)
--- analyse/haltepunkte.py
+++ analyse/haltepunkte.py
@@ -1,23 +1,16 @@
 import analyse.compare
 
 
-def make_simba_code(stops, code_field="CODE", field="stop_id"):
-    stops["is_simba"] = stops.line.str.match(r'\d{3}-[A-Z]-.*')
-    stops[code_field] = None
-    stops.loc[stops.is_simba, code_field] = stops[stops.is_simba][field].apply(lambda x: x.split("_")[1])
-    return stops
-
-
-def ea(runs):
-    def func(df, run):
-        df = make_simba_code(df)[["CODE", "boarding", "alighting"]].copy()
+def ea(runs, func):
+    def _func(df, run):
+        df = func(df, run)
         df.loc[:, "boarding"] = df.boarding * run.scale_factor
         df.loc[:, "alighting"] = df.alighting * run.scale_factor
         return df
 
     df = analyse.compare._compare_by(runs, "CODE",
                                      lambda r: r.get_stops(),
-                                     func=func,
+                                     func=_func,
                                      values=["boarding", "alighting"], aggfunc="sum")
     return df
 
