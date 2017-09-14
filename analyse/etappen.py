3d77b4219bd79617791bba6949718db0c99e7829
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-09-14 14:13:46 +0200

    scale wege and etappe

--- analyse/etappen.py
+++ analyse/etappen.py
@@ -3,6 +3,9 @@ from analyse.compare import _compare_by
 
 def pfahrt(runs, by, prozentual=False, func=None):
     df = _compare_by(runs, by, lambda r: r.get_legs(), func=func, values="trip_id", aggfunc="count")
+
+    df = df.multiply([r.scale_factor for r in runs])
+
     if prozentual:
         df = df.div(df.sum())*100
     return df
@@ -10,6 +13,9 @@ def pfahrt(runs, by, prozentual=False, func=None):
 
 def pkm(runs, by, prozentual=False, func=None):
     df = _compare_by(runs, by, lambda r: r.get_legs(), func=func, values="distance", aggfunc="sum")/1000.0
+
+    df = df.multiply([r.scale_factor for r in runs])
+
     if prozentual:
         df = df.div(df.sum())*100
     return df
