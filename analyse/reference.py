73126d8d8da7ec4ac30b477f21a65552e241a074
Métrailler Denis (P-UE-NAE-ETM) <denis.metrailler@sbb.ch>
2017-12-16 08:56:26 +0100

    first implementation of reporting and references

---
+++ analyse/reference.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from variable import *
+import analyse.run
+
+path_count_linkvolumes = r"/opt/sbb/hdd/u222223/MATSim/simulations/CH/zaehldaten/link_count_data_astra15.csv"
+path_mikro = r"/opt/sbb/hdd/u222223/MATSim/mikrozensus/2015/20171212_Input_MZMV_Kalibrationsvergleich.csv"
+path_count_ea = r"/opt/sbb/hdd/u222223/MATSim/simulations/CH/zaehldaten/stop_count_data_fqkal+14_alles.csv"
+
+
+class Reference:
+    def __init__(self, path_astra, path_mikro, path_ea):
+        self.path_astra = path_astra
+        self.path_mikro = path_mikro
+        self.path_ea = path_ea
+
+    def get_mzmv_run(self, subpopulation="regular_inAct"):
+        df = pd.read_csv(self.path_mikro, sep=",", dtype={"link_id": str})
+        df[SUBPOPULATION] = subpopulation
+        df = df.rename(columns={u'Weglaenge': "distance"})
+        df.distance = df.distance * 1000.0
+        mzmv = analyse.run.Run(name="mzmv")
+        mzmv.data["journeys"] = df
+        return mzmv
+
+    def get_stations(self):
+        ref_ea = pd.read_csv(self.path_ea, sep=";")
+        ref_ea[CODE] = ref_ea.stop_id.apply(lambda x: x.split("_")[-1])
+
+        ref_ea = ref_ea.groupby(CODE)[["boarding"]].sum()
+        ref_ea = ref_ea.rename(columns={"boarding": "FQKal+"})
+        return ref_ea
+
+    def get_count_stations(self):
+        ref_astra = pd.read_csv(self.path_astra, sep=";", dtype={"link_id": str})
+        ref_astra.rename(columns={"zaehlstellen_bezeichnung": "name"}, inplace=True)
+        return ref_astra
---
+++ analyse/reporting.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import io
+from PIL import Image
+from variable import *
+
+
+class SheetData:
+    def __init__(self, df, fig, name):
+        self.df = df
+        self.fig = fig
+        self.name = name
+
+
+def make_report(runs, filename, ref=None):
+    datas = get_datas(runs=runs, ref=ref)
+    _make_report(datas=datas, filename=filename)
+
+
+def _make_report(datas, filename):
+    buffers = []
+
+    def get_buffer(fig):
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png', dpi=100, bbox_inches='tight')
+        buf.seek(0)
+        im = Image.open(buf)
+        im.show()
+        buffers.append(buf)
+        return buf
+
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+
+    worksheet = writer.book.add_worksheet("TableOfContents")
+    for i, data in enumerate(datas):
+        worksheet.write_url(i + 1, 1, url="internal:'%s'!A1" % data.name, string=data.name)
+
+    for data in datas:
+        df = data.df
+        df.to_excel(writer, sheet_name=data.name, startrow=1, startcol=18)
+        buf = get_buffer(data.fig)
+        writer.sheets[data.name].insert_image('A1', "aa", {'image_data': buf})
+
+    writer.save()
+
+    for buf in buffers:
+        buf.close()
+
+
+def get_datas(runs, ref):
+    datas = []
+
+    df, ax = runs.plot_nb_trips(by=MAIN_MODE, foreach=SUBPOPULATION)
+    datas.append(SheetData(df, ax, "#trips 1"))
+
+    df, ax = runs.plot_nb_trips(by=MAIN_MODE, foreach=[SUBPOPULATION, SEASON_TICKET])
+    datas.append(SheetData(df, ax, "#trips 2"))
+
+    df, fig = runs.plot_nb_trips(by=MAIN_MODE, foreach=[RAUMTYP, SUBPOPULATION])
+    datas.append(SheetData(df, fig, "#trips 3"))
+
+    df, ax = runs.plot_pkm_trips(by=MAIN_MODE, foreach=SUBPOPULATION)
+    datas.append(SheetData(df, ax, "pkm trips 1"))
+
+    df, ax = runs.plot_pkm_trips(by=MAIN_MODE, foreach=[SUBPOPULATION, SEASON_TICKET])
+    datas.append(SheetData(df, ax, "pkm trips 2"))
+
+    df, fig = runs.plot_pkm_trips(by=MAIN_MODE, foreach=[RAUMTYP, SUBPOPULATION])
+    datas.append(SheetData(df, fig, "pkm trips 3"))
+
+    df, fig = runs.plot_pkm_distr_trips(foreach=[MAIN_MODE], percent=True, rotate=True)
+    datas.append(SheetData(df, fig, "pkm distribution"))
+
+    df, fig = runs.plot_pkm_distr_trips(foreach=[MAIN_MODE, SUBPOPULATION, SEASON_TICKET], percent=True,
+                                        rotate=True)
+    datas.append(SheetData(df, fig, "pkm distribution 2"))
+
+    df, fig = runs.plot_pkm_distr_legs(foreach=[IS_SIMBA_FQ], percent=True, inverse_percent_axis=False, rotate=True)
+    datas.append(SheetData(df, fig, "simba_legs"))
+
+    df, fig = runs.plot_einsteiger(by=CODE, codes=ref.get_stations().codes, ref_df=ref.get_stations())
+    datas.append(SheetData(df, fig, "Einsteiger"))
+
+    df, fig = runs.plot_einsteiger(by=CODE, codes=ref.get_stations().codes, foreach=SUBPOPULATION)
+    datas.append(SheetData(df, fig, "Einsteiger 2"))
+
+    df, fig = runs.plot_vehicles(by="name", names=ref.get_count_stations().names, ref_df=ref.get_stations())
+    datas.append(SheetData(df, fig, "Link counts"))
+
+    return datas
