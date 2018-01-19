14fa47b12ec2710103c48bc34a2c88d4eefd00ca
Métrailler Denis (P-V-APL-MEP) <denis.metrailler@sbb.ch>
2018-01-19 07:03:39 +0100

    log line number

--- analyse/run.py
+++ analyse/run.py
@@ -61,7 +61,7 @@ def _start_logging():
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
