ab278201d8c25c631af8b9d57cc347b522bba134
Johannes Lieberherr <johannes.lieberherr@sbb.ch>
2018-01-22 22:43:35 +0100

    stop-ids as str in order to avoid float-ids

--- analyse/skims.py
+++ analyse/skims.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import logging
 from analyse.variable import IS_SIMBA, JOURNEY_ID, TRIP_ID, BOARDING_STOP, START_TIME, ALIGHTING_STOP, END_TIME, \
-    STOP_ID, PF
+    STOP_ID, PF, DISTANCE
 from vimapy.helpers import hhmmss_to_seconds, seconds_to_hhmmss
 
 pd.options.mode.chained_assignment = None
@@ -82,37 +82,37 @@ def set_simba_binnenverkehr_fq_attributes(df_legs, stop_ids_perimeter, stop_ids_
 
 
 def get_station_to_station_skims(df_legs):
-    start_time_per_journey = df_legs[["journey_id", "start_time_first_stop"]].groupby("journey_id").min()
+    start_time_per_journey = df_legs[[JOURNEY_ID, "start_time_first_stop"]].groupby(JOURNEY_ID).min()
     start_time_per_journey = start_time_per_journey.reset_index()
-    end_time_per_journey = df_legs[["journey_id", "end_time_last_stop"]].groupby("journey_id").max()
+    end_time_per_journey = df_legs[[JOURNEY_ID, "end_time_last_stop"]].groupby(JOURNEY_ID).max()
     end_time_per_journey = end_time_per_journey.reset_index()
 
-    skim_per_journey = start_time_per_journey.merge(end_time_per_journey, on="journey_id")
+    skim_per_journey = start_time_per_journey.merge(end_time_per_journey, on=JOURNEY_ID)
     skim_per_journey["bz"] = skim_per_journey["end_time_last_stop"] - skim_per_journey["start_time_first_stop"]
     skim_per_journey["bz"] = skim_per_journey["bz"].apply(lambda x: x if x >= 0 else x + 24 * 60 * 60)
 
-    uh_per_journey = df_legs[["journey_id", "start_time"]].groupby("journey_id").count()
+    uh_per_journey = df_legs[[JOURNEY_ID, "start_time"]].groupby(JOURNEY_ID).count()
     uh_per_journey = uh_per_journey.reset_index()
-    uh_per_journey.columns = ["journey_id", "uh"]
+    uh_per_journey.columns = [JOURNEY_ID, "uh"]
     uh_per_journey["uh"] -= 1.0
-    skim_per_journey = skim_per_journey.merge(uh_per_journey, on="journey_id")
+    skim_per_journey = skim_per_journey.merge(uh_per_journey, on=JOURNEY_ID)
 
-    pf_per_journey = df_legs[["journey_id", PF]].groupby("journey_id").min()
+    pf_per_journey = df_legs[[JOURNEY_ID, PF]].groupby(JOURNEY_ID).min()
     pf_per_journey = pf_per_journey.reset_index()
-    skim_per_journey = skim_per_journey.merge(pf_per_journey, on="journey_id")
+    skim_per_journey = skim_per_journey.merge(pf_per_journey, on=JOURNEY_ID)
 
-    distance_per_journey = df_legs[["journey_id", "distance"]].groupby("journey_id").sum()
+    distance_per_journey = df_legs[[JOURNEY_ID, DISTANCE]].groupby(JOURNEY_ID).sum()
     distance_per_journey = distance_per_journey.reset_index()
-    skim_per_journey = skim_per_journey.merge(distance_per_journey, on="journey_id")
+    skim_per_journey = skim_per_journey.merge(distance_per_journey, on=JOURNEY_ID)
 
     skim_per_journey["weighted_bz"] = skim_per_journey[PF] * skim_per_journey["bz"]
     skim_per_journey["weighted_uh"] = skim_per_journey[PF] * skim_per_journey["uh"]
-    skim_per_journey["weighted_distance"] = skim_per_journey[PF] * skim_per_journey["distance"]
+    skim_per_journey["weighted_distance"] = skim_per_journey[PF] * skim_per_journey[DISTANCE]
 
-    first_last_stop_per_journey = df_legs[["journey_id", "first_stop", "last_stop"]].groupby(
-        "journey_id").min()
+    first_last_stop_per_journey = df_legs[[JOURNEY_ID, "first_stop", "last_stop"]].groupby(
+        JOURNEY_ID).min()
     first_last_stop_per_journey = first_last_stop_per_journey.reset_index()
-    skim_per_journey = skim_per_journey.merge(first_last_stop_per_journey, on="journey_id")
+    skim_per_journey = skim_per_journey.merge(first_last_stop_per_journey, on=JOURNEY_ID)
 
     skim_per_station_to_station = skim_per_journey[
         ["first_stop", "last_stop", PF, "weighted_bz", "weighted_uh", "weighted_distance"]].groupby(
